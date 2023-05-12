8f137653e07c74b5d36f93ff7f010a5eb79a33f8
hanhafizh <farhanhafizh770@gmail.com>
2023-05-12 21:29:24 +0700

    Indexed DB ( Liked Movie )

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,16 @@
 import UrlParser from "../../routes/url-parser";
 import RestoSource from "../../data/resto-source";
-import { createRestaurantDetailTemplate } from "../templates/template-creator";
+import {
+  createRestaurantDetailTemplate,
+  createLikeButtonTemplate,
+} from "../templates/template-creator";
+import LikeButtonInitiator from "../../utils/like-button-initiator";
 
 const DetailPages = {
   async render() {
     return `
     <div id="restaurant" class="restaurant"></div>
+    <div id="likeButtonContainer"></div>
       `;
   },
 
@@ -14,7 +19,21 @@ const DetailPages = {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const restaurant = await RestoSource.RestoDetail(url.id);
     const restaurantContainer = document.querySelector("#restaurant");
+    const likeButtonContainer = document.querySelector("#likeButtonContainer");
     restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
+    likeButtonContainer.innerHTML = createLikeButtonTemplate();
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector("#likeButtonContainer"),
+      movie: {
+        id: url.id,
+        name: restaurant.name,
+        pictureId: restaurant.pictureId,
+        city: restaurant.city,
+        description: restaurant.description,
+        rating: restaurant.rating,
+      },
+    });
   },
 };
 
